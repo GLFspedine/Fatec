dd68cf1a8af5ec6f27177e737a1490a91823c921
GLFspedine <gustavo.ferreira81@fatec.sp.gov.br>
2024-03-25 20:45:36 -0300

    aula06

--- algoritimos/aula06/exercicio03.py
+++ algoritimos/aula06/exercicio03.py
@@ -4,4 +4,20 @@ a1 = (l * 2.8) * 2
 a2 = (c * 2.8) * 2
 ap = 2.10 * 0.8
 lt = (a1 + a2 - ap) / 3
+t1 = 1
+t2 = 3.7
+t3 = 18
 print("Serão necessarios: ", lt, "litros de tinta para pintar o aposento")
+if (lt / 1) <= 1:
+    print('comprar 1 lata de 1 litro')
+elif (lt / 1) <= 3:
+    c = int((lt / 1) + 1)
+    print('comprar', c, 'lata de 1 litro')
+elif (lt / 3.7) <= 1:
+    print('comprar 1 lata de 3.7 litro')
+elif (lt / 3.7) <= 4:
+    c = int((lt / 3.7) + 1)
+    print('comprar', c, 'lata de 3,7 litros')
+elif (lt / 18) >= 1:
+    c = int((lt / 18) + 1)
+    print('comprar', c, 'lata de 18 litros')
