4b1cbdef94636fdbef1514f5b4d0dbf66c84326c
GLFspedine <gustavo.ferreira81@fatec.sp.gov.br>
2024-03-25 21:10:29 -0300

    aula06

--- algoritimos/aula06/exercicio02.py
+++ algoritimos/aula06/exercicio02.py
@@ -3,9 +3,13 @@ if v <= 0:
     print('Valor incorrento!')
 elif v <= 1000:
     c = v * 0.1
+    des = v - c
+    print("Seu desconto foi de R$: ", c, 'e o valor de sua compra final é R$: ', des)
 elif v <= 5000:
     c = v * 0.2
+    des = v - c
+    print("Seu desconto foi de R$: ", c, 'e o valor de sua compra final é R$: ', des)
 else:
     c = v * 0.3
-des = v - c
-print("Seu desconto foi de R$: ", c, 'e o valor de sua compra final é R$: ', des)
+    des = v - c
+    print("Seu desconto foi de R$: ", c, 'e o valor de sua compra final é R$: ', des)
