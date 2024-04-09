767177752ba300b26b1f620a628ff7941d389159
GLFspedine <gustavo.ferreira81@fatec.sp.gov.br>
2024-04-08 22:09:55 -0300

    aula08

--- algoritimos/aula08/exercicio05.py
+++ algoritimos/aula08/exercicio05.py
@@ -1,6 +1,17 @@
-palavra = input(f"Digite uma palavra: ").upper()
+frase = input(f"Digite uma palavra: ").upper()
+frase1 = frase.strip()
+palavras = frase1.split()
+frase2 = ' '
+for palavra in palavras:
+    frase2 = frase2 + palavra
+frase3 = frase2[::-1]
+if frase3 == frase2:
+    palindromo = True
 
-if palavra == (palavra[::-1]):
+else:
+    palindromo = False
+
+if palindromo:
     print("é um Palindromo")
 else:
     print("não é um Palindromo")
